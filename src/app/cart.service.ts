7ebaa747ee98046b20546ad3a2c379669511e34b
ginocampra <gino@gcampra.com.br>
2023-02-01 10:26:08 -0300

    5 files modified, 8 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
 import { Injectable } from '@angular/core';
-
+/* . . . */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  /* . . . */
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 
-  constructor() { }
+  items: Product[] = [];
+  /* . . . */
+
+  constructor(private http: HttpClient) {}
+  /* . . . */
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  /* . . . */
 }
